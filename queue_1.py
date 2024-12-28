d0e2a0a9130b800fdf2be2ff0637e76583c3541f
daniellee6925 <147388870+daniellee6925@users.noreply.github.com>
2024-12-28 11:41:51 -0800

    add practice

--- queue_1.py
+++ queue_1.py
@@ -17,6 +17,9 @@ class Queue:
     def dequeue(self):
         return self.buffer.pop()
 
+    def front(self):
+        return self.buffer[-1]
+
     def is_empty(self):
         return len(self.buffer) == 0
 
@@ -42,10 +45,26 @@ def serve_order():
         time.sleep(2)
 
 
+def print_binary(n):
+    q = Queue()
+    q.enqueue("1")
+
+    for i in range(n):
+        front = q.front()
+        print(" ", front)
+        q.enqueue(front + "0")
+        q.enqueue(front + "1")
+        q.dequeue()
+
+
 if __name__ == "__main__":
+    """
     orders = ["pizza", "samosa", "pasta", "fries", "burger"]
     # multithreading
     t1 = threading.Thread(target=place_order, args=(orders,))
     t2 = threading.Thread(target=serve_order)
     t1.start()
     t2.start()
+    """
+
+    print_binary(10)
